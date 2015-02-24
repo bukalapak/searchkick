4d2abcfcd1aee5007199976b7f8fe3a499d99e33
Nugroho Herucahyono <xinuc@xinuc.org>
2015-02-24 19:44:18 +0700

    Only index ActiveRecord

--- lib/searchkick/reindex_v2_job.rb
+++ lib/searchkick/reindex_v2_job.rb
@@ -4,6 +4,7 @@ module Searchkick
 
     def perform(klass, id)
       model = klass.constantize
+      return unless model.superclass == ActiveRecord::Base
       record = model.unscoped.where(id: id).first
       index = model.searchkick_index
       index.store record if record
