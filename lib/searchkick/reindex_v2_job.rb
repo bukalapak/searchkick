1c6171cbc03a42d0c6ebc5fb38996747b88bbc5e
Nugroho Herucahyono <xinuc@xinuc.org>
2015-03-20 17:58:27 +0700

    add timeout to reindex job

--- lib/searchkick/reindex_v2_job.rb
+++ lib/searchkick/reindex_v2_job.rb
@@ -5,9 +5,11 @@ module Searchkick
     def perform(klass, id)
       model = klass.constantize
       return unless model.superclass == ActiveRecord::Base
-      record = model.unscoped.where(id: id).first
-      index = model.searchkick_index
-      index.store record if record
+      Timeout.timeout(5) do
+        record = model.unscoped.where(id: id).first
+        index = model.searchkick_index
+        index.store record if record
+      end
     end
 
   end
